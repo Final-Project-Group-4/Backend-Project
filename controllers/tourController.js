6e85136bb9c00b6ce7e42419bbe9ec3ae9d73fe1
Nathalia Padron <nbavutti@gmail.com>
2022-11-22 13:31:33 +0100

    Added the create tour controller deleted the protected route middleware on the import tour route changed the name for the usermodel (expect that this will result in a merge conflict) but we checked along with Kubra so, I got the correct name. My App is  working fine now.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -13,6 +13,16 @@ export const getAllTours = async (req, res, next) => {
   }
 };
 export const getSingleTour = async (req, res, next) => {};
-export const createTour = async (req, res, next) => {};
+export const createTour = async (req, res, next) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: newTour,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 export const updateTour = async (req, res, next) => {};
 export const deleteTour = async (req, res, next) => {};
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -6,15 +6,15 @@ import {
   createTour,
   updateTour,
 } from "../controllers/tourController.js";
-import { protectController } from "../middleware/protectController.js";
+//import { protectController } from "../middleware/protectController.js";
 
 const router = express.Router();
 
 router.get("/", getAllTours); //get all the tours
-router.post("/", protectController, createTour); // to create a new tour
+router.post("/", createTour); // to create a new tour
 
 router.get("/:id", getSingleTour); // To get a single tour by sending the tourId
-router.put("/:id", protectController, updateTour); // To update a single tour by sending the tourId
-router.delete("/:id", protectController, deleteTour); // To delete a single tour by sending the tourId
+router.put("/:id", updateTour); // To update a single tour by sending the tourId
+router.delete("/:id", deleteTour); // To delete a single tour by sending the tourId
 
 export default router;

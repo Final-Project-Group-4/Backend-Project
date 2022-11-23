579e8b13ab90067239627f305e0f33f4e5829f45
Nathalia Padron <nbavutti@gmail.com>
2022-11-23 12:29:28 +0100

    update user finished

--- controllers/authController.js
+++ controllers/authController.js
@@ -77,17 +77,11 @@ export const login = async (req, res) => {
     token,
   });
 };
-/* 
+
 export const logout = async (req, res) => {
-  res.cookie("jwt", "loggedout", {
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true,
-  });
-  res.status(200).json({
-    status: "success",
-  });
+ 
 };
- */
+ 
 export const updateUser = async (req, res, next) => {
   try {
     const user_id = req.params.id;

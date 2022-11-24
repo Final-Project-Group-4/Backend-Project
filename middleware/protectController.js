9d46d0c70fe66db8231cedeaba4428100f9d51be
rekha1801 <rekha.musuvati@gmail.com>
2022-11-24 08:54:05 +0100

    Added reset password functionality

--- middleware/protectController.js
+++ middleware/protectController.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import { promisify } from "util";
+import UserModel from "../models/userModel.js";
 
 export const protectController = (req, res, next) => {
   //We will take the token from request.headers.authorization and split [1] as token.
@@ -23,4 +25,42 @@ export const protectController = (req, res, next) => {
   }
 
   //if token is not valid we will send a bad request response back to the front end
+};
+
+export const protectController1 = async (req, res, next) => {
+  //1) Get the token and check if its there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+    console.log(token);
+    if (!token) {
+      return res.status(401).json({
+        message: "You are not logged in! Please log in to get access.",
+      });
+    }
+  }
+
+  //2) Verification of token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  //changed the password after the token is issued
+  //3) Check if user still exists
+  const currentUser = await UserModel.findById(decoded.id);
+  if (!currentUser) {
+    return res.status(401).json({
+      message: "The user belonging to the token no longer exists",
+    });
+  }
+
+  //4) Check if user changed password after the token was issued.
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return res.status(401).json({
+      message: "User recently changed password. Please login again",
+    });
+  }
+  req.user = currentUser;
+  next();
 };
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -6,24 +6,19 @@ import {
   createTour,
   updateTour,
 } from "../controllers/tourController.js";
+import { protectController1 } from "../middleware/protectController.js";
 
 //import { protectController } from "../middleware/protectController.js";
 
-
-
-
 const router = express.Router();
 
-router.get("/", getAllTours); //get all the tours
+router.get("/", protectController1, getAllTours); //get all the tours
 router.post("/", createTour); // to create a new tour
 
 //protectController removed as it is not allowing to test in postman
 router.get("/:id", getSingleTour); // To get a single tour by sending the tourId
 
-
-router.put("/:id",  updateTour); // To update a single tour by sending the tourId
-router.delete("/:id",  deleteTour); // To delete a single tour by sending the tourId
-
-
+router.put("/:id", updateTour); // To update a single tour by sending the tourId
+router.delete("/:id", deleteTour); // To delete a single tour by sending the tourId
 
 export default router;

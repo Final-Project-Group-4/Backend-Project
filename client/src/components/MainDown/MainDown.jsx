92b14b0f57635553a8279c5c9fb56c3160c5a5d2
rekha1801 <rekha.musuvati@gmail.com>
2022-12-05 11:28:27 +0100

    config.scss added

--- client/src/components/MainDown/MainDown.jsx
+++ client/src/components/MainDown/MainDown.jsx
@@ -1,9 +1,25 @@
-import "./_MainDown.scss"
+import "./_MainDown.scss";
+import ReactPlayer from "react-player";
 
 function MainDown() {
   return (
-    <div>MainDown</div>
-  )
+    <div className="maindown">
+      <div className="tours">
+        <div className="hiking">Hiking</div>
+        <div className="safari">Safari</div>
+        <div className="coffee">Coffee</div>
+        <div className="custom">Custom Tour</div>
+      </div>
+
+      <div className="videos">
+        Videos about Kilimanjaro
+        <ReactPlayer url="https://youtu.be/7MZoIN9I8oU" />
+        {/* <ReactPlayer url="https://youtu.be/XRwrpXf5V6c" />
+        <ReactPlayer url="https://youtu.be/TsnAxKIYvmQ" />
+        <ReactPlayer url="https://youtu.be/h5sG-A7Sk-c" /> */}
+      </div>
+    </div>
+  );
 }
 
-export default MainDown
+export default MainDown;

4ec6a0cec02a7c0d9a17153c29ca9ff595f6360c
rekha1801 <rekha.musuvati@gmail.com>
2022-12-07 11:44:16 +0100

    Added YouTubeEmbed component and css properties

--- client/src/components/MainDown/MainDown.jsx
+++ client/src/components/MainDown/MainDown.jsx
@@ -1,21 +1,21 @@
 import "./_MainDown.scss";
-
-import ReactPlayer from "react-player";
+import React from "react";
+import YouTubeEmbed from "./YouTubeEmbed.jsx";
 
 function MainDown() {
   return (
     <div className="container">
       <div className="tours">
-        <div className="hiking">Hiking</div>
-        <div className="safari">Safari</div>
-        <div className="coffee">Coffee</div>
-        <div className="custom">Custom Tour</div>
+        <div className="hiking col-12-s col-4-m col-2-l">Hiking</div>
+        <div className="safari col-12-s col-4-m col-2-l">Safari</div>
+        <div className="coffee col-12-s col-4-m col-2-l">Coffee</div>
+        <div className="custom col-12-s col-4-m col-2-l">Custom Tour</div>
       </div>
 
       <div className="videos">
-        <p>WHY TRAVEL WITH US?</p>
+        <p className="videoTitle">WHY TRAVEL WITH US?</p>
         <div className="player">
-          <ReactPlayer url="https://youtu.be/7MZoIN9I8oU" />
+          <YouTubeEmbed embedId="7MZoIN9I8oU" />
         </div>
       </div>
     </div>

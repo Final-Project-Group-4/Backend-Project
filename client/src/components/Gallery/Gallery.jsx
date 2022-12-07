ae7dba755aafa77f88167b046ce682c99e6a935a
rekha1801 <rekha.musuvati@gmail.com>
2022-12-07 17:08:06 +0100

    Changes to resolve errors after merging

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -1,73 +1,80 @@
-import React from 'react'
+import React from "react";
 import "./_Gallery.scss";
-import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faCircleChevronLeft,faCircleChevronRight,faCircleXmark} from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleChevronLeft,
+  faCircleChevronRight,
+  faCircleXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
+export default function Gallery({ galleryImages }) {
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [openModal, setOpenModal] = useState(true);
 
-export default function Gallery({galleryImages}) {
-
-    const [slideNumber,setSlideNumber] = useState(0)
-    const [openModal,setOpenModal] = useState(true)
-
-    const handleOpenModal = (index) => {
-        setSlideNumber(index)
-        setOpenModal(true)
-    }
-// close modal
-    const handleCloseModal = () => {
-        setOpenModal(false)
-    }
-// Previous image
-    const prevSlide = () => {
-        slideNumber === 0 ? setSlideNumber(galleryImages.length -1) : setSlideNumber(slideNumber -1)
-    }
-// Next image
-    const nextSlide = () => {
-        slideNumber +1 === galleryImages.length ? setSlideNumber(0) : setSlideNumber(slideNumber +1)
-    }
-
+  const handleOpenModal = (index) => {
+    setSlideNumber(index);
+    setOpenModal(true);
+  };
+  // close modal
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+  // Previous image
+  const prevSlide = () => {
+    slideNumber === 0
+      ? setSlideNumber(galleryImages.length - 1)
+      : setSlideNumber(slideNumber - 1);
+  };
+  // Next image
+  const nextSlide = () => {
+    slideNumber + 1 === galleryImages.length
+      ? setSlideNumber(0)
+      : setSlideNumber(slideNumber + 1);
+  };
 
   return (
     <div>
-        <h1>Gallery</h1>
-        {openModal && 
-            <div className='sliderWrap'>
-
-                <FontAwesomeIcon icon={faCircleXmark} className='btnClose' onClick={handleCloseModal} />
-                <FontAwesomeIcon icon={faCircleChevronLeft} className='btnPrev' onClick={prevSlide}  />
-                <FontAwesomeIcon icon={faCircleChevronRight} className='btnNext' onClick={nextSlide}  />
-                <div className='fullScreenImage' >
-                    <img src={galleryImages[slideNumber].img} alt='image' />
-                </div>
-            </div>
-        }
-    <div className='gallery-wrap'>
-        {
-            galleryImages && galleryImages.map((slide,index) => {
-                return(
-                    <div className='single'
-                     key={index}
-                     onClick={()=> handleOpenModal(index)}
-                     >
-                        <img src={slide.img} alt='' />
-                    </div>
-                )
-            })
-        }
+      <h1>Gallery</h1>
+      {openModal && (
+        <div className="sliderWrap">
+          <FontAwesomeIcon
+            icon={faCircleXmark}
+            className="btnClose"
+            onClick={handleCloseModal}
+          />
+          <FontAwesomeIcon
+            icon={faCircleChevronLeft}
+            className="btnPrev"
+            onClick={prevSlide}
+          />
+          <FontAwesomeIcon
+            icon={faCircleChevronRight}
+            className="btnNext"
+            onClick={nextSlide}
+          />
+          <div className="fullScreenImage">
+            <img src={galleryImages[slideNumber].img} alt="safari" />
+          </div>
         </div>
+      )}
+      <div className="gallery-wrap">
+        {galleryImages &&
+          galleryImages.map((slide, index) => {
+            return (
+              <div
+                className="single"
+                key={index}
+                onClick={() => handleOpenModal(index)}
+              >
+                <img src={slide.img} alt="" />
+              </div>
+            );
+          })}
+      </div>
     </div>
-  )
+  );
 }
-
-
-
-
-
-
-
-
-
 
 // const buildURL = imagePath =>
 //   `https://assets.imgix.net/tutorials/${imagePath}.webp`;
--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -1,6 +1,10 @@
-import "./_Footer.scss"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faInstagram , faTwitter } from "@fortawesome/free-brands-svg-icons";
+import "./_Footer.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
@@ -26,9 +30,18 @@ function Footer() {
           <div className="col">
             <h4>FAQ</h4>
             <ul className="list-unstyled">
-              <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum ad rem necessitatibus dolor molestiae aliquid sequi officia .</li>
-              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi blanditiis aspernatur, voluptas!</li>
-              <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus adipisci!</li>
+              <li>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum
+                ad rem necessitatibus dolor molestiae aliquid sequi officia .
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+                blanditiis aspernatur, voluptas!
+              </li>
+              <li>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Voluptatibus adipisci!
+              </li>
             </ul>
           </div>
         </div>
@@ -36,26 +49,26 @@ function Footer() {
         <div className="last-row">
           <p className="col-sm">copyright 2009 | All rights reserved</p>
           <div className="social-icons">
-           <div className="icons">  
-            <a href="https://facebook.com">
-              <FontAwesomeIcon icon={faFacebook}/>
-            </a>
-          </div>
-          <div className="icons">
-            <a href="https://instagram.com">
-              <FontAwesomeIcon icon={faInstagram}/>
-            </a>
-          </div>
-          <div className="icons">
-            <a href="https://twitter.com">
-              <FontAwesomeIcon icon={faTwitter}/>
-            </a>
-          </div>
+            <div className="icons">
+              <a href="https://facebook.com">
+                <FontAwesomeIcon icon={faFacebook} />
+              </a>
+            </div>
+            <div className="icons">
+              <a href="https://instagram.com">
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
+            </div>
+            <div className="icons">
+              <a href="https://twitter.com">
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;

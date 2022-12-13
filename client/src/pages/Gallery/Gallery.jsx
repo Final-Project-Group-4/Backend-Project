c814633d0b1444247dc50eebd150d027dc9c5986
Dimosthenis22 <dimosthenisk80@gmail.com>
2022-12-13 10:34:53 +0100

    Gallery page created

--- client/src/pages/Gallery/Gallery.jsx
+++ client/src/pages/Gallery/Gallery.jsx
@@ -1,5 +1,86 @@
 import React from "react";
+import "./Gallery.scss";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleChevronLeft,
+  faCircleChevronRight,
+  faCircleXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { Contacts, Footer, Navbar } from "../../components/export";
 
-export default function Gallery() {
-  return <div>Gallery</div>;
+export default function Gallery({ galleryImages }) {
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [openModal, setOpenModal] = useState(true);
+
+  // Modal handler
+  const handleOpenModal = (index) => {
+    setSlideNumber(index);
+    setOpenModal(true);
+  };
+  // close modal
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+  // Previous image
+  const prevSlide = () => {
+    slideNumber === 0
+      ? setSlideNumber(galleryImages.length - 1)
+      : setSlideNumber(slideNumber - 1);
+  };
+  // Next image
+  const nextSlide = () => {
+    slideNumber + 1 === galleryImages.length
+      ? setSlideNumber(0)
+      : setSlideNumber(slideNumber + 1);
+  };
+
+  return (
+    <div className="container">
+      <Navbar />
+      <h3>Photo gallery</h3>
+      
+      <div className="gallery-wrap">
+        {galleryImages &&
+          galleryImages.map((slide, index) => {
+            return (
+              <div
+                className="single"
+                key={index}
+                onClick={() => handleOpenModal(index)}
+              >
+                <img src={slide.img} alt="" />
+              </div>
+            );
+          })}
+      </div>
+
+      {openModal && (
+        <div className="sliderWrap">
+          <FontAwesomeIcon
+            icon={faCircleXmark}
+            className="btnClose"
+            onClick={handleCloseModal}
+          />
+          <FontAwesomeIcon
+            icon={faCircleChevronLeft}
+            className="btnPrev"
+            onClick={prevSlide}
+          />
+          <FontAwesomeIcon
+            icon={faCircleChevronRight}
+            className="btnNext"
+            onClick={nextSlide}
+          />
+          <div className="fullScreenImage">
+            <img src={galleryImages[slideNumber].img} alt="safari" />
+          </div>
+        </div>
+      )}
+        <Contacts />
+          <Footer />
+    </div>
+  );
 }
+
+

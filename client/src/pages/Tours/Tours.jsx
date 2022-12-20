2442b2ebb0fe091cf6e00a2befa1adcee426f10e
Nathalia Padron <nbavutti@gmail.com>
2022-12-20 21:50:30 +0100

    changed the tourspage, added an option that opens a single tour window when theres only one tour of that type in the categories, added pictures on each tour card, fixed the height and added responsiveness

--- client/src/pages/Tours/Tours.jsx
+++ client/src/pages/Tours/Tours.jsx
@@ -15,7 +15,6 @@ export default function Tours() {
   //Get all the Tours data and check for the status
   const loadToursData = async () => {
     const allTours = await axios.get(`http://localhost:4000/api/tours`);
-
     if (allTours.status === 200) {
       setTourData(allTours.data.data);
     } else {
@@ -26,9 +25,14 @@ export default function Tours() {
   //Get the Tours data according to the type of the tour and check for the status
   const loadToursDataByType = async () => {
     const filteredToursByType = await axios.get(`http://localhost:4000/api/tours/category/${type}`);
-    //console.log(filteredToursByType.data);
+    console.log(filteredToursByType.data);
     if (filteredToursByType.status === 200) {
       setTourData(filteredToursByType.data);
+      //this if statement is to redirect to the tour page if there is only one tour of that type
+      if (filteredToursByType.data.length === 1) {
+        console.log('its JUST ONE');
+        window.location.href = `/tours/${filteredToursByType.data[0]._id}`;
+      }
     } else {
       console.error('Something went wrong');
     }
@@ -39,41 +43,52 @@ export default function Tours() {
   useEffect(() => {
     if (type) {
       loadToursDataByType();
+      window.scrollTo(0, 0);
     } else {
       loadToursData();
+      window.scrollTo(0, 0);// add this line to scroll to the top when you render your components 
     }
   }, [type]);
 
   return (
     <div className="container1">
-      <Container
-        sx={{ position: 'relative', top: '120px', marginBottom: '8em', minHeight: '95vh' }}
-      >
-        <Grid
-          container
-          spacing={4}
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          textAlign="center"
-        >
-          {tourData.map((tour) => {
-            return (
-              <Grid item xs={3} margin="0.7em">
-                <TourCard
-                  days={tour.days}
-                  name={tour.name}
-                  tour={tour.name}
-                  subtitle={tour.subtitle}
-                  difficulty={tour.difficulty}
-                  scenery={tour.scenery}
-                  id={tour._id}
-                />
-              </Grid>
-            );
-          })}
-        </Grid>
-      </Container>
+      <div className='container2'>
+        <div className='leftSide'>
+          <img style={{height:"200px", width:"auto"}} src={require("http://thenewcode.com/assets/images/responsive-design-comparison.jpg")} alt="" />
+        </div>
+        <Container
+        className='rightSide' 
+         >
+        
+          <Grid className='gridRight'
+          sx={{ position: 'relative', top:"80px", marginBottom:"80px", overflowY:"scroll"}}
+            container
+            spacing={1}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            textAlign="center"
+          >
+            {tourData.map((tour) => {
+              return (
+                <Grid item xs={12} sm={12} md={12} lg={5} margin="0.2em">
+                  <TourCard
+                    mainImg={tour.imgCover}
+                    days={tour.days}
+                    name={tour.name}
+                    tour={tour.name}
+                    subtitle={tour.subtitle}
+                    difficulty={tour.difficulty}
+                    scenery={tour.scenery}
+                    id={tour._id}
+                  />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Container>
+        
+      </div>
       <Contacts />
     </div>
   );

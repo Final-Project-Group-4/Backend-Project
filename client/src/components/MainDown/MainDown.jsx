2d3bc912061b86fb1fd9fd78793de9645a10a796
joseph.shami <josephshami3@gmail.com>
2023-01-05 15:38:57 +0100

    translation part

--- client/src/components/MainDown/MainDown.jsx
+++ client/src/components/MainDown/MainDown.jsx
@@ -1,42 +1,43 @@
 import './_MainDown.scss';
 import React from 'react';
 import axios from 'axios';
+import { useTranslation } from 'react-i18next';
 import YouTubeEmbed from './YouTubeEmbed.jsx';
 import { Link } from 'react-router-dom';
 
 function MainDown() {
 
-
+  const {t} = useTranslation();
   return (
     <div className="container mainDown">
       <div className="tours">
         <Link to="/tours/category/hiking">
           <div className="hiking col-12-s col-4-m col-2-l" id="hiking">
-            Hiking
+            {t("Hiking")}
           </div>
         </Link>
 
         <Link to="/tours/category/safari">
           <div className="safari col-12-s col-4-m col-2-l" id="safari">
-            Safari
+           {t("Safari")}
           </div>
         </Link>
 
         <Link to="/tours/category/coffee">
           <div className="coffee col-12-s col-4-m col-2-l" id="coffee">
-            Coffee
+            {t("Coffee")}
           </div>
         </Link>
 
         <Link to="/plantrip">
           <div className="custom col-12-s col-4-m col-2-l" id="customTour">
-            Custom Tour
+            {t("CustomTour")}
           </div>
         </Link>
       </div>
 
       <div className="videos">
-        <p className="videoTitle">WHY TRAVEL WITH US?</p>
+        <p className="videoTitle">{t("WhyTravelWithUs")}</p>
         <div className="player">
           <YouTubeEmbed embedId="7MZoIN9I8oU" />
         </div>
--- client/src/components/MainUp/MainUp.jsx
+++ client/src/components/MainUp/MainUp.jsx
@@ -1,51 +1,56 @@
 import { ValueCard, Peter } from '../export';
 import './_MainUp.scss';
+import { useTranslation } from 'react-i18next';
 import { FaCoffee, FaBinoculars, FaCarSide } from 'react-icons/fa';
 import mainUp from './../../assets/lp-img-mainup.jpg';
 import { HiScissors } from 'react-icons/hi';
 
-function MainUp() {
-  const values = [
-    {
-      title: 'Custom trip',
-      text: 'We offer best of Tanzania adventures: Kilimanjaro trekking, Meru trekking, day hike, safaris to different tanzanian parks.',
-      icon: <HiScissors size={60} />,
-    },
-    {
-      title: 'Local, Coffee',
-      text: 'We make coffee in a traditional way from a coffee seed to a cup of coffee, while doing this process we also sing and dance.',
-      icon: <FaCoffee size={60} />,
-    },
-    {
-      title: 'Safari',
-      text: 'We offer best of Tanzania safari: Great migration, Ngorongoro crater, Tarangire national park and other Tanzanian parks.',
-      icon: <FaBinoculars size={60} />,
-    },
-    {
-      title: 'Transportation',
-      text: 'We use 4x4 drive Land Cruisers for safaris and mini bus to transfer from airport to the hotel.',
-      icon: <FaCarSide size={60} />,
-    },
-  ];
+function MainUp(){ 
 
+const {t} = useTranslation();
+// we have used the same data in values inside the json part the only problem is that we can not paste the icon
+// const values = [
+//   {
+//   title: 'Custom trip',
+//   text: 'We offer best of Tanzania adventures: Kilimanjaro trekking, Meru trekking, day hike, safaris to different tanzanian parks.',
+//   icon: <HiScissors size={60} />,
+//   },
+//   {
+//   title: 'Local, Coffee',
+//   text: 'We make coffee in a traditional way from a coffee seed to a cup of coffee, while doing this process we also sing and dance.',
+//   icon: <FaCoffee size={60} />,
+//   },
+//   {
+//   title: 'Safari',
+//   text: 'We offer best of Tanzania safari: Great migration, Ngorongoro crater, Tarangire national park and other Tanzanian parks.',
+//   icon: <FaBinoculars size={60} />,
+//   },
+//   {
+//   title: 'Transportation',
+//   text: 'We use 4x4 drive Land Cruisers for safaris and mini bus to transfer from airport to the hotel.',
+//   icon: <FaCarSide size={60} />,
+//   },
+//   ]; 
   return (
     <div className="container mainup">
       <div className="first">
         <div className="text">
-          <h2>We are offering</h2>
+          <h2>{t("whatWeOffer")}</h2>
           <p>
-            Mlay Tours leads tourists on incredible adventures in Tanzania. Leading treks to Mt.
-            Kilimanjaro Mt. Meru, Tanzania's game parks and cultural tours.All these activities will
-            make your journey unforgettable.
+          {t("weOffer")}
           </p>
         </div>
         <img src={mainUp} alt="kilimanjaro" />
       </div>
       <div className="values-container">
-        {values.map((item, i) => (
+        {t("values", {returnObjects:true}).map((item, i) => (
           <ValueCard key={i} title={item.title} text={item.text} icon={item.icon} />
         ))}
+   
+
+
       </div>
+
       <Peter />
     </div>
   );

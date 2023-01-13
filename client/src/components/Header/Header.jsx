3bfe2c25cdee0bc29fd72a9d383b93c40e29143b
Nathalia Padron <nbavutti@gmail.com>
2023-01-13 23:49:41 +0100

    fixed responsiveness on the main down, and header, added logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,20 +2,25 @@ import './_Header.scss';
 import { useTranslation } from 'react-i18next';
 import image from './mountain.jpg';
 import { useNavigate } from 'react-router-dom';
-import logo from "../../assets/mlay_tours.png"
+
 
 function Header() {
   const navigate = useNavigate();
   const {t} = useTranslation();
   return (
     <div className="main__div">
-      <img src={image} alt="elephant" />
-      <header className="main__header">
-        <img src={require("../../assets/mlay_tours.png")}></img>
-        <h2>{t("welcomeTo")}</h2>
-        <p>{t("ourGoal")}</p>
-        <button onClick={() => navigate('/tours')}>{t("ourTours")}</button>
-      </header>
+      
+      
+        <img className='bg-imgEl' src={image} alt="elephant" />
+        <header className="main__header">
+        <div className='overlay'>
+          <h2>{t("welcomeTo")}</h2>
+          <img src={require("../../assets/Mlay_version2.png")}></img>
+          <p>{t("ourGoal")}</p>
+          <button className='btn-OurTours' onClick={() => navigate('/tours')}>{t("ourTours")}</button>
+        </div>
+        </header>
+    
     </div>
   );
 }

1b5c2d3e31172a2eaa77ec3d336cb7e22dd4479f
Nathalia Padron <nbavutti@gmail.com>
2023-01-16 11:31:43 +0100

    changes

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,28 +2,26 @@ import './_Header.scss';
 import { useTranslation } from 'react-i18next';
 import image from './mountain.jpg';
 import { useNavigate } from 'react-router-dom';
-import { FaArrowRight } from "react-icons/fa";
-
+import { FaArrowRight } from 'react-icons/fa';
 
 function Header() {
   const navigate = useNavigate();
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   return (
     <div className="main__div">
-      
-      
-        <img className='bg-imgEl' src={image} alt="elephant" />
-        <header className="main__header">
+      <img className="bg-imgEl" src={image} alt="elephant" />
+      <header className="main__header">
         {/* <div className='overlay'> */}
-          <h2>{t("welcomeTo")}</h2>
-          <img src={require("../../assets/Mlay_version4.png")} alt="logo"></img>
-          
-          <p>{t("ourGoal")}</p>
-           <li><a className='linkToTours' href="http://localhost:3000/tours">{t("ourTours")} <FaArrowRight /></a></li>  
-          <button className='btn-OurTours' onClick={() => navigate('/tours')}>{t("ourTours")}</button>
-       {/*  </div> */}
-        </header>
-    
+        <h2>{t('welcomeTo')}</h2>
+        <img src={require('../../assets/Mlay_version4.png')} alt="logo"></img>
+
+        <p>{t('ourGoal')}</p>
+        {/* <li><a className='linkToTours' href="http://localhost:3000/tours">{t("ourTours")} <FaArrowRight /></a></li>   */}
+        <button className="btn-OurTours" onClick={() => navigate('/tours')}>
+          {t('ourTours')}
+        </button>
+        {/*  </div> */}
+      </header>
     </div>
   );
 }

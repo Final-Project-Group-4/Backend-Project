f970fa2b271af234b81b0d55d70fa6de55556580
Adanurk <gumussoykbr@gmail.com>
2023-01-25 13:52:27 +0100

    cleaning

--- client/src/components/shared/Navbar/mainNav/MainNav.jsx
+++ client/src/components/shared/Navbar/mainNav/MainNav.jsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import './mainNav.scss';
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import "./mainNav.scss";
 
 function MainNav() {
   const { t } = useTranslation();
@@ -8,19 +8,19 @@ function MainNav() {
   return (
     <ul className="app__navbar__links">
       <li className="app__flex p-text">
-        <Link to={'/'}>Home</Link>
+        <Link to={"/"}>Home</Link>
       </li>
       <li className="app__flex p-text">
-        <Link to={'/tours'}>{t('tours')}</Link>
+        <Link to={"/tours"}>{t("tours")}</Link>
       </li>
       <li className="app__flex p-text">
-        <Link to={'/gallery'}>{t('gallery')}</Link>
+        <Link to={"/gallery"}>{t("gallery")}</Link>
       </li>
       <li className="app__flex p-text">
-        <Link to={'/about'}>{t('aboutUs')}</Link>
+        <Link to={"/about"}>{t("aboutUs")}</Link>
       </li>
       <li className="app__flex p-text">
-        <Link to={'/plantrip'}>{t('planTrip')}</Link>
+        <Link to={"/plantrip"}>{t("planTrip")}</Link>
       </li>
     </ul>
   );
